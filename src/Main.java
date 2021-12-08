83a106a1e37005cc300bcb8d29c028fcace28a94
Darya Bychenkova <by4enkova1@gmail.com>
2021-12-08 20:41:21 +0300

    Main.java is modified

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int initBalance = 100;
-        int sum = 1100;
+        int sum = 110;
         int bonus = sum / 100;
-        int overallBalance = initBalance + sum + bonus;
-        if (sum >= 1000)
+        if (sum >= 1000) {
             bonus = sum / 100;
-        else
+        } else {
             bonus = 0;
+        }
+        int overallBalance = initBalance + sum + bonus;
+
         System.out.println(overallBalance);
-    }}
+    }
+}
